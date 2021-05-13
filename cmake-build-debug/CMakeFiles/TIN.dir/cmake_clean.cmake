275bacff8e907bf600b16feda2784a4febbf3eac
aleokr <aleksandra.okrutny1@gmail.com>
2021-05-13 19:21:28 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/TIN.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TIN.dir/main.c.o"
+  "CMakeFiles/TIN.dir/source/socekt.c.o"
+  "TIN"
+  "TIN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TIN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/socket.h
@@ -0,0 +1,8 @@
+//
+// Created by ola on 13.05.2021.
+//
+
+#ifndef TIN_SOCKET_H
+#define TIN_SOCKET_H
+
+#endif //TIN_SOCKET_H
