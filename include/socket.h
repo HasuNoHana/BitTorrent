88a068706e0c2f58dbe379048c553588856bfbfb
aleokr <aleksandra.okrutny1@gmail.com>
2021-05-13 21:27:17 +0200

    Socket architecture - init

--- include/socket.h
+++ include/socket.h
@@ -1,8 +1,23 @@
-//
-// Created by ola on 13.05.2021.
-//
-
 #ifndef TIN_SOCKET_H
 #define TIN_SOCKET_H
 
-#endif //TIN_SOCKET_H
+#include <sys/types.h>
+#include <sys/socket.h>
+
+//create socket, define address etc., return socket Descriptor
+int createSocket();
+
+//close socket
+void closeSocket(int socketDescriptor);
+
+int listenToConnect();
+
+int acceptConnection(int socketDescriptor);
+
+int connectToDifferentSocket(int socketDescriptor);
+
+void sendData(int socketDescriptor, char *buf);
+
+char *receiveData(int socketDescriptor);
+
+#endif
---
+++ source/socket.c
@@ -0,0 +1,30 @@
+#include "../include/socket.h"
+
+int createSocket(){
+    return -1;
+}
+
+void closeSocket(int socketDescriptor){
+
+}
+
+int listenToConnect(){
+    return -1;
+}
+
+int acceptConnection(int socketDescriptor){
+    return -1;
+}
+
+int connectToDifferentSocket(int socketDescriptor){
+    return -1;
+}
+
+void sendData(int socketDescriptor, char *buf){
+
+}
+
+char *receiveData(int socketDescriptor){
+    char * data;
+    return data;
+}
