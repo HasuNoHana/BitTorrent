a3e00b0ac5fa9e641544da8b3898925bf612e784
Jan Wojciech Such <j.such@stud.elka.pw.edu.pl>
2021-06-01 18:46:04 +0000

    Upload New File

---
+++ include/queue.h
@@ -0,0 +1,24 @@
+#ifndef TIN_QUEUE_H
+#define TIN_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define MSG_LENGTH 64
+#define MSG_ARRAY_SIZE 32
+#define MODULE_COUNT 4
+
+extern pthread_mutex_t writemsg_lock[MODULE_COUNT];
+
+extern char msg_array[MODULE_COUNT][MSG_ARRAY_SIZE][MSG_LENGTH];
+extern int msg_read[MODULE_COUNT];
+extern int msg_write[MODULE_COUNT];
+
+int writemsg(int ID, char *message);
+int readmsg(int ID, char *buffer);
+int prepareQueueMutexes();
+
+#endif
